ba2c78966eba7e7ee3558877c83d0cf324b6fc82
Evan D <edogariu@princeton.edu>
2023-12-28 17:45:15 -0500

    made pip installable

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='meta_opt',
+   version='1.0',
+   description='for running meta optimization',
+   author='Evan Dogariu',
+   packages=['meta_opt'],  #same as name
+   install_requires=['wheel',], #external packages as dependencies
+)
