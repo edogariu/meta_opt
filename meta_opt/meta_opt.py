ec7d43d2be5ab32ee4571417fea79d804de7e181
Evan Dogariu! <78564140+edogariu@users.noreply.github.com>
2024-01-13 05:06:03 -0500

    Update meta_opt.py

--- meta_opt/meta_opt.py
+++ meta_opt/meta_opt.py
@@ -88,10 +88,12 @@ def update(cstate,
     grads = _grad_fn(cstate.M, cstate.H, cstate.HH, initial_tstate, disturbances, batches)
     
     # clip grads
-    K = 5.
-    if isinstance(cstate.M, jnp.ndarray): 
+    if isinstance(cstate.M, jnp.ndarray):  # if scalar M's
+        K = 5.0
         grads = (jnp.clip(grads[0], -K, K),)
-    else: grads = (jax.tree_map(lambda g: jnp.clip(g, -K, K), grads[0]),)
+    else: 
+        K = 0.05
+        grads = (jax.tree_map(lambda g: jnp.clip(g, -K, K), grads[0]),)
     
     updates, new_opt_state = cstate.tx.update(grads, cstate.opt_state, (cstate.M,))
     M = optax.apply_updates(cstate.M, updates[0])
