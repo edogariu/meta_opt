9c93830f4fc5aa6ca0af285dd85b4cd286eb419e
Evan D <edogariu@princeton.edu>
2024-03-11 19:01:34 -0400

    rdy for della

---
+++ scripts/main_mnist_fullbatch.py
@@ -0,0 +1,76 @@
+# handle the system stuff, colab stuff, etc
+import os
+DIR = os.path.abspath(".")
+
+# make sure we have the necessary folders
+for subdir in ['data', 'figs', 'datasets']: 
+    temp = os.path.join(DIR, subdir)
+    if not os.path.isdir(temp): os.mkdir(temp)
+
+from meta_opt.train_loops import train_standard_opt, train_hgd, train_meta_opt
+from meta_opt.utils.experiment_utils import make, save_checkpoint, process_results, bcolors, plot, get_final_cparams
+import meta_opt.configs as configs
+
+import re
+import matplotlib.pyplot as plt
+import numpy as np
+import dill as pkl
+import optax
+
+# ==================================================
+# configuration and seeds for each trial
+SEEDS = [0, 1, 2]
+
+NAME = 'mnist_fullbatch'
+CFG = {
+    # training options
+    'workload': 'MNIST',
+    'num_iters': 20000,
+    'eval_every': 200,
+    'num_eval_iters': -1,
+    'batch_size': 512,
+    'full_batch': False,
+    'reset_every': 10000,
+
+    # experiment options
+    'experiment_name': NAME,
+    'load_checkpoint': False,
+    'overwrite': True,  # whether to allow us to overwrite existing checkpoints or throw errors
+    'directory': DIR,
+}
+
+def run(seeds, cfg):
+    processed_results = pkl.load(open('{}/data/cifar_fullbatch_processed.pkl'.format(cfg['directory']), 'rb'))
+    initial_cparams = get_final_cparams(processed_results, 'cf')
+    results = make(cfg)
+    
+    # uncomment the ones to run, with correctly chosen hyperparameters
+    for s in seeds:
+        CFG['seed'] = s
+        print(f'running with seed {s}')
+        
+        # ours
+        opt = optax.inject_hyperparams(optax.sgd)(learning_rate=2e-4)
+        results['cf'].append(train_meta_opt(CFG, counterfactual=True, H=32, HH=2, meta_optimizer=opt, initial_lr=0.1, cparams_initial=initial_cparams))
+        results['frozen'].append(train_meta_opt(CFG, counterfactual=False, H=32, HH=1, meta_optimizer=optax.inject_hyperparams(optax.sgd)(learning_rate=0), initial_lr=0.1, cparams_initial=initial_cparams))
+        results['ncf'].append(train_meta_opt(CFG, counterfactual=False, H=32, HH=2, meta_optimizer=opt, initial_lr=0.1,  cparams_initial=initial_cparams))
+
+        # standard benchmarks
+        benchmarks = {
+            'sgd': optax.inject_hyperparams(optax.sgd)(learning_rate=0.2),
+            'momentum': optax.chain(optax.add_decayed_weights(1e-4), optax.inject_hyperparams(optax.sgd)(learning_rate=0.1, momentum=0.9)),
+            'adamw': optax.inject_hyperparams(optax.adamw)(learning_rate=1e-3, b1=0.9, b2=0.999, weight_decay=1e-4),
+            # 'rmsprop': optax.inject_hyperparams(optax.rmsprop)(learning_rate=1e-3),
+        }
+        for k, opt in benchmarks.items(): results[k].append(train_standard_opt(CFG, opt))
+
+        # other
+        results['hgd'].append(train_hgd(CFG, initial_lr=0.1, hypergrad_lr=1e-3))
+
+        save_checkpoint(CFG, results, checkpoint_name=f'seed {s}')
+    processed_results = process_results(CFG, results)
+# ==================================================
+
+
+if __name__ == '__main__':
+    run(SEEDS, CFG)
---
+++ scripts/slurm.py
@@ -0,0 +1,35 @@
+"""
+FOR SLURM SCRIPTS
+"""
+# handle the system stuff, colab stuff, etc
+import os
+DIR = os.path.abspath(".")
+
+# make sure we have the necessary folders
+for subdir in ['data', 'figs', 'datasets']: 
+    temp = os.path.join(DIR, subdir)
+    if not os.path.isdir(temp): os.mkdir(temp)
+
+from meta_opt.train_loops import train_standard_opt, train_hgd, train_meta_opt
+from meta_opt.utils.experiment_utils import make, save_checkpoint, process_results, bcolors, plot, get_final_cparams
+import meta_opt.configs as configs
+
+import re
+import matplotlib.pyplot as plt
+import numpy as np
+import dill as pkl
+import optax
+
+
+# ==================================================
+from main_cifar_pretrained import CFG, run
+# ==================================================
+
+
+SEEDS = range(500)
+if __name__ == '__main__':
+    idx = int(os.environ["SLURM_ARRAY_TASK_ID"])
+    CFG['experiment_name'] = f'{NAME}_{idx}'
+    s = SEEDS[idx]
+    
+    run([s,], CFG)    
