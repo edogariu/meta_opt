542c8763f63ecfd9e1b90075dbd31d366e047aa4
Evan D <edogariu@princeton.edu>
2024-03-14 11:56:30 -0400

    rdy for della

--- slurm.py
+++ slurm.py
@@ -10,16 +10,6 @@ for subdir in ['data', 'figs', 'datasets']:
     temp = os.path.join(DIR, subdir)
     if not os.path.isdir(temp): os.mkdir(temp)
 
-from meta_opt.train_loops import train_standard_opt, train_hgd, train_meta_opt
-from meta_opt.utils.experiment_utils import make, save_checkpoint, process_results, bcolors, plot, get_final_cparams
-import meta_opt.configs as configs
-
-import re
-import matplotlib.pyplot as plt
-import numpy as np
-import dill as pkl
-import optax
-
 
 # ==================================================
 from scripts.main_wmt_baselines import CFG, run
