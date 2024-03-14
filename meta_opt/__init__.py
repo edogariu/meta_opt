0478a95fd5da2462f1f6242e6947e44ad9546aaf
Evan D <edogariu@princeton.edu>
2024-03-14 15:21:01 -0400

    cleaned

--- meta_opt/__init__.py
+++ meta_opt/__init__.py
@@ -2,11 +2,11 @@
 import os
 try:
     from google import colab  # for use in google colab!!
-    !git clone https://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@github.com/edogariu/meta-opt
-    !pip install -q ./meta-opt
-    !pip install -q dill
+    os.system('git clone https://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@github.com/edogariu/meta-opt')
+    os.system('pip install -q ./meta-opt')
+    os.system('pip install -q dill')
     # !pip install -q jax[cuda12_pip]==0.4.20 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html  # for disabling prealloc, see https://github.com/google/jax/discussions/19014
-    !pip install -q tensorflow-text ml_collections clu sentencepiece  # for WMT
+    os.system('pip install -q tensorflow-text ml_collections clu sentencepiece')  # for WMT
     from google.colab import drive
     drive.mount('/content/drive')
     DIR = os.path.abspath("./drive/My Drive/meta-opt")
