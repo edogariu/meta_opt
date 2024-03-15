fbf27a2505a3b5a9ef292c6eb9a2fb3dde8b5cd3
Evan D <edogariu@princeton.edu>
2024-03-15 17:06:19 -0400

    added wmt

--- meta_opt/workloads/__init__.py
+++ meta_opt/workloads/__init__.py
@@ -32,7 +32,6 @@ def get_workload(cfg, optimizer):
     elif cfg['workload'] == 'WMT':
         train_ds, test_ds, example_input, loss_fn, metric_fns, tokenizer = load_wmt(cfg, dataset_dir=os.path.join(directory, 'datasets'))
         model = WMT(cfg, tokenizer, size=cfg['transformer_size'])
-        if cfg['full_batch']: raise NotImplementedError('full batch on WMT')
     else:
         raise NotImplementedError(cfg['workload'])
 
