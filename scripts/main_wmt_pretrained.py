6ad1fba2f4ccab4660dbc726c6aa8706667eb3b9
Evan D <edogariu@princeton.edu>
2024-03-17 13:27:46 -0400

    add wmt fullbatch

--- scripts/main_wmt_pretrained.py
+++ scripts/main_wmt_pretrained.py
@@ -45,11 +45,14 @@ def run(seeds, cfg):
         processed_results = pkl.load(open('{}/data/wmt_fullbatch_processed.pkl'.format(cfg['directory']), 'rb'))
         initial_cparams = get_final_cparams(processed_results, 'ncf_adam')
         results = make(cfg)
-        opt = optax.inject_hyperparams(optax.sgd)(learning_rate=0)
+        # opt = optax.inject_hyperparams(optax.sgd)(learning_rate=0)
         # results['ncf_adam_frozen'].append(train_meta_opt(CFG, counterfactual=False, H=8, HH=1, meta_optimizer=opt, initial_lr=1.0, cparams_initial=initial_cparams))
         # save_checkpoint(CFG, results, checkpoint_name=f'seed {s}')
-        opt = optax.inject_hyperparams(optax.adam)(learning_rate=4e-4)
-        results['ncf_adam'].append(train_meta_opt(CFG, counterfactual=False, H=8, HH=3, meta_optimizer=opt, initial_lr=1.0, cparams_initial=initial_cparams))
+        # opt = optax.inject_hyperparams(optax.adam)(learning_rate=4e-4)
+        # results['ncf_adam'].append(train_meta_opt(CFG, counterfactual=False, H=8, HH=3, meta_optimizer=opt, initial_lr=1.0, cparams_initial=initial_cparams))
+        # save_checkpoint(CFG, results, checkpoint_name=f'seed {s}')
+        opt = optax.inject_hyperparams(optax.sgd)(learning_rate=2e-4)
+        results['ncf'].append(train_meta_opt(CFG, counterfactual=False, H=8, HH=3, meta_optimizer=opt, initial_lr=1.0, cparams_initial=initial_cparams))
         save_checkpoint(CFG, results, checkpoint_name=f'seed {s}')
         
         # standard benchmarks
