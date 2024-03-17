8ff807a051ff2cf2bd770f13718563784627a523
Evan D <edogariu@princeton.edu>
2024-03-17 18:22:39 -0400

    add wmt fullbatch;

--- scripts/main_wmt_fullbatch.py
+++ scripts/main_wmt_fullbatch.py
@@ -27,7 +27,7 @@ CFG = {
     'transformer_size': 'base',
     
     # experiment options
-    'experiment_name': 'wmt_fullbatch_resetquick',
+    'experiment_name': 'wmt_fullbatch_64',
     'load_checkpoint': True,
     'overwrite': True,  # whether to allow us to overwrite existing checkpoints or throw errors
     'directory': DIR,
