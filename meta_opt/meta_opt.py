6716c53e75ad0cbeca4d2116c02031499badf3f2
Evan D <edogariu@princeton.edu>
2024-03-17 21:31:13 -0400

    gonna try and make cf fast

--- meta_opt/meta_opt.py
+++ meta_opt/meta_opt.py
@@ -226,22 +226,45 @@ class MetaOpt:
                         tstate,  # tstate after a step of gd
                         grads,  # grads from the step of gd that resulted in `tstate`
                         batch,  # batch from step of gd that resulted in `tstate`
-                        ):      
-        # lazy initialize the history if it is still `None``
-        if self.batch_history is None: self.batch_history = {k: [v for _ in range(self.HH)] for k, v in batch.items()}
+                        ):   
+        
+        @jax.jit
+        def _prologue(cstate, grad_history, batch_history, tstate, grads, batch):
+            if batch_history is None: batch_history = {k: [v for _ in range(self.HH)] for k, v in batch.items()}
+             # clip disturbances (K = 10 is very soft)
+            K = 10; grads = jax.tree_map(lambda g: jnp.clip(g, -K, K), grads)
+            grad_history = jax.tree_map(append, grad_history, grads)
+            control = compute_control(cstate.cparams, slice_pytree(grad_history, cstate.HH, cstate.H))  # use past H disturbances
+            tstate = tstate.replace(params=add_pytrees(tstate.params, control))
+            return grad_history, batch_history, control, tstate
+    
+    
+        @jax.jit
+        def _epilogue(tstate_history, batch_history, tstate, batch):
+            tstate_history = append(tstate_history, tstate)
+            for k in batch_history.keys(): batch_history[k] = append(batch_history[k], batch[k]) 
+            return tstate_history, batch_history
+    
+    
+        # # lazy initialize the history if it is still `None``
+        # if self.batch_history is None: self.batch_history = {k: [v for _ in range(self.HH)] for k, v in batch.items()}
 
-        # clip disturbances (K = 10 is very soft)
-        K = 10; grads = jax.tree_map(lambda g: jnp.clip(g, -K, K), grads)
+        # # clip disturbances (K = 10 is very soft)
+        # K = 10; grads = jax.tree_map(lambda g: jnp.clip(g, -K, K), grads)
                      
-        self.grad_history = jax.tree_map(append, self.grad_history, grads)
-        control = compute_control(self.cstate.cparams, slice_pytree(self.grad_history, self.cstate.HH, self.cstate.H))  # use past H disturbances
-        tstate = tstate.replace(params=add_pytrees(tstate.params, control))
+        # self.grad_history = jax.tree_map(append, self.grad_history, grads)
+        # control = compute_control(self.cstate.cparams, slice_pytree(self.grad_history, self.cstate.HH, self.cstate.H))  # use past H disturbances
+        # tstate = tstate.replace(params=add_pytrees(tstate.params, control))
+        
+        self.grad_history, self.batch_history, control, tstatae = _prologue(self.cstate, self.grad_history, self.batch_history, tstate, grads, batch)
 
         if self.t >= self.cstate.H + self.cstate.HH:
             self.cstate = counterfactual_update(self.cstate, self.tstate_history[0], self.grad_history, self.batch_history, batch)
+        
+        self.tstate_history, self.batch_history = _epilogue(self.tstate_history, self.batch_history, tstate, batch)
             
-        self.tstate_history = append(self.tstate_history, tstate)
-        for k in self.batch_history.keys(): self.batch_history[k] = append(self.batch_history[k], batch[k]) 
+        # self.tstate_history = append(self.tstate_history, tstate)
+        # for k in self.batch_history.keys(): self.batch_history[k] = append(self.batch_history[k], batch[k]) 
         self.t += 1
         return tstate
     
