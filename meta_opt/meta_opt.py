328f0ec858ea50c4eba548aecc3e9ad3e960ed8a
Evan D <edogariu@princeton.edu>
2024-03-17 21:35:35 -0400

    gonna try and make cf fast

--- meta_opt/meta_opt.py
+++ meta_opt/meta_opt.py
@@ -222,7 +222,7 @@ class MetaOpt:
         return tstate, (loss, index_pytree(self.grad_history, -1))
     
     @jax.jit
-    def _prologue(cstate, grad_history, batch_history, tstate, grads, batch):
+    def prologue(cstate, grad_history, batch_history, tstate, grads, batch):
         if batch_history is None: batch_history = {k: [v for _ in range(self.HH)] for k, v in batch.items()}
             # clip disturbances (K = 10 is very soft)
         K = 10; grads = jax.tree_map(lambda g: jnp.clip(g, -K, K), grads)
@@ -233,7 +233,7 @@ class MetaOpt:
 
 
     @jax.jit
-    def _epilogue(tstate_history, batch_history, tstate, batch):
+    def epilogue(tstate_history, batch_history, tstate, batch):
         tstate_history = append(tstate_history, tstate)
         for k in batch_history.keys(): batch_history[k] = append(batch_history[k], batch[k]) 
         return tstate_history, batch_history
@@ -255,12 +255,12 @@ class MetaOpt:
         # control = compute_control(self.cstate.cparams, slice_pytree(self.grad_history, self.cstate.HH, self.cstate.H))  # use past H disturbances
         # tstate = tstate.replace(params=add_pytrees(tstate.params, control))
         
-        self.grad_history, self.batch_history, control, tstatae = _prologue(self.cstate, self.grad_history, self.batch_history, tstate, grads, batch)
+        self.grad_history, self.batch_history, control, tstatae = prologue(self.cstate, self.grad_history, self.batch_history, tstate, grads, batch)
 
         if self.t >= self.cstate.H + self.cstate.HH:
             self.cstate = counterfactual_update(self.cstate, self.tstate_history[0], self.grad_history, self.batch_history, batch)
         
-        self.tstate_history, self.batch_history = _epilogue(self.tstate_history, self.batch_history, tstate, batch)
+        self.tstate_history, self.batch_history = epilogue(self.tstate_history, self.batch_history, tstate, batch)
             
         # self.tstate_history = append(self.tstate_history, tstate)
         # for k in self.batch_history.keys(): self.batch_history[k] = append(self.batch_history[k], batch[k]) 
