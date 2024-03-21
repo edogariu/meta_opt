53e5b2d323865a1cea08342a4962edfb785f5c53
Evan D <edogariu@princeton.edu>
2024-03-20 23:44:19 -0400

    add baselines

--- scripts/wmt_baselines.py
+++ scripts/wmt_baselines.py
@@ -46,11 +46,11 @@ def run(seeds, cfg):
             # 'rsqrt_0.002': rsqrt(lr=0.002, warmup_steps=4000),
             # 'sgd_2.0': optax.inject_hyperparams(optax.sgd)(learning_rate=2.0),
             # 'sgd_1.0': optax.inject_hyperparams(optax.sgd)(learning_rate=1.0),
-            'momentum_1.0': optax.chain(optax.add_decayed_weights(1e-4), optax.inject_hyperparams(optax.sgd)(learning_rate=2.0, momentum=0.9)),
-            'momentum_2.0': optax.chain(optax.add_decayed_weights(1e-4), optax.inject_hyperparams(optax.sgd)(learning_rate=1.0, momentum=0.9)),
+            # 'momentum_1.0': optax.chain(optax.add_decayed_weights(1e-4), optax.inject_hyperparams(optax.sgd)(learning_rate=2.0, momentum=0.9)),
+            # 'momentum_2.0': optax.chain(optax.add_decayed_weights(1e-4), optax.inject_hyperparams(optax.sgd)(learning_rate=1.0, momentum=0.9)),
             # 'adamw': optax.inject_hyperparams(optax.adamw)(learning_rate=1e-3, b1=0.9, b2=0.999, weight_decay=1e-4),
-            # 'dadamw': optax.inject_hyperparams(optax.contrib.dadapt_adamw)(),
-            # 'mechadamw': optax.contrib.mechanize(optax.inject_hyperparams(optax.adamw)(learning_rate=1e-3, b1=0.9, b2=0.999, weight_decay=1e-4)),
+            'dadamw': optax.inject_hyperparams(optax.contrib.dadapt_adamw)(),
+            'mechadamw': optax.contrib.mechanize(optax.inject_hyperparams(optax.adamw)(learning_rate=1e-3, b1=0.9, b2=0.999, weight_decay=1e-4)),
             # 'rmsprop': optax.inject_hyperparams(optax.rmsprop)(learning_rate=1e-3),
         }
         for k, opt in benchmarks.items(): 
