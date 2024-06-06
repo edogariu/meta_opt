74fe3860270b51ae03d9d11a553a1b1d3d68011f
Evan Dogariu <evandogariu@Evans-Air-2.home>
2024-06-06 04:57:18 -0400

    literally rewrote the whole codebase

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
    name='meta_opt',
    version='1.0',
    description='for running meta optimization',
-   author='Evan Dogariu',
+   author='someone anonymous :)  (we submitted the code with the paper and are under review)',
    packages=find_packages(),
-   install_requires=['wheel',], #external packages as dependencies
+   install_requires=['wheel',],  #external packages as dependencies
 )
