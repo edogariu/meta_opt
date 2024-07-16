8dca0f444dc13ad530e475c75ad85b0e85a6b992
Evan Dogariu! <78564140+edogariu@users.noreply.github.com>
2024-07-16 00:41:15 -0400

    Update test.py

--- configs/test.py
+++ configs/test.py
@@ -26,11 +26,11 @@ def get_config():
         experimental_setup='init2winit',
         workload_name='mnist', 
         full_batch=False,  # whether to do full gradient descent on one batch (fixed during the whole training) or regular minibatch SGD
-        num_episodes=20,
-        num_iters=500,  # if None, uses default for the workload
+        num_episodes=1,
+        num_iters=5000,  # if None, uses default for the workload
 
         # backend details
-        num_batch_devices=1,
+        num_batch_devices=8,
         num_opt_devices=1,
 
         # how often to do things
@@ -44,25 +44,27 @@ def get_config():
         log_every=50,
         use_wandb=False)
 
-    # optimizer_cfg = sgd.SGDConfig(learning_rate=0.01, momentum=0.9, nesterov=False, weight_decay=None, grad_clip=None)
+    optimizer_cfg = sgd.SGDConfig(learning_rate=None, momentum=None, nesterov=False, weight_decay=None, grad_clip=None)
     # optimizer_cfg = adamw.AdamWConfig(learning_rate=0.001, b1=0.9, b2=0.999, eps=1e-8, weight_decay=None, grad_clip=None)
 
     # meta_optimizer_cfg = SGDConfig(learning_rate=1e-5, momentum=0, nesterov=False, weight_decay=None, grad_clip=None)
-    meta_optimizer_cfg = adamw.AdamWConfig(learning_rate=4e-4, b1=0.9, b2=0.999, eps=1e-8, weight_decay=0, grad_clip=None)
-    optimizer_cfg = metaopt.MetaOptConfig(base_learning_rate=0.001, weight_decay=1e-4, grad_clip=None,
-                                H=16, HH=2, m_method='scalar', scale_by_adam_betas=None, 
-                                fake_the_dynamics=False, freeze_gpc_params=False, freeze_cost_fn_during_rollouts=False,
-                                meta_optimizer_cfg=meta_optimizer_cfg, use_bfloat16=False)
-
-    # meta_optimizer_cfg = sgd.SGDConfig(learning_rate=1e-5, momentum=0, nesterov=False, weight_decay=None, grad_clip=None)
+    # meta_optimizer_cfg = adamw.AdamWConfig(learning_rate=4e-4, b1=0.9, b2=0.999, eps=1e-8, weight_decay=0, grad_clip=None)
     # optimizer_cfg = metaopt.MetaOptConfig(base_learning_rate=0.001, weight_decay=1e-4, grad_clip=None,
-    #                               H=16, HH=2, m_method='diagonal', scale_by_adam_betas=(0.9, 0.999),
-    #                               meta_optimizer_cfg=meta_optimizer_cfg, meta_grad_clip=10.0)
+    #                             H=16, HH=2, m_method='scalar', scale_by_adam_betas=None, 
+    #                             fake_the_dynamics=False, freeze_gpc_params=False, freeze_cost_fn_during_rollouts=False,
+    #                             meta_optimizer_cfg=meta_optimizer_cfg, use_bfloat16=False)
+
+    sweep = []
+    for lr in [1e-4, 1e-3, 1e-2, 1e-1]:
+        for momentum in [0., 0.8, 0.9, 0.99]:
+            for nesterov in [True, False]:
+                for wd in [1e-4, 1e-3]:
+                    sweep.append((experiment_cfg, optimizer_cfg.replace(learning_rate=lr, momentum=momentum, nesterov=nesterov, weight_decay=wd))
     
     if experiment_cfg.experimental_setup == 'algoperf':
-        return experiment_cfg, optimizer_cfg
+        return sweep
     elif experiment_cfg.experimental_setup == 'init2winit':
         assert IS_INTERNAL, 'havent set up init2winit on external yet'
-        return config_utils.convert_configs(experiment_cfg, optimizer_cfg, base_config.get_base_config())
+        return config_utils.convert_configs(sweep, base_config.get_base_config())
     else:
         raise NotImplementedError(experiment_cfg.experimental_setup)
